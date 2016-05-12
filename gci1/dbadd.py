e7c0586a9c6c3845f93047b519afc97363ca431d
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-05-12 21:39:31 +0900

    push some changes of gci1/kadai1.py

--- gci1/dbadd.py
+++ gci1/dbadd.py
@@ -6,18 +6,9 @@ import csvkit
 dbcon = mysql.connector.connect(database=config.db, user=config.user, password=config.passwd, host=config.host)
 dbcur = dbcon.cursor()
 
-# dataset = [
-#     ('138297393', '38979', "^_^"),
-#     ('298379847', '38979', "(-  - ;)"),
-#     ('928398329', '88298',"((o_o;;;))))"),
-#     ('382809389', '113918',"m(__)m"),
-# ]
 csv_data = csvkit.reader(file('rating_final.csv'))
 for row in csv_data:
-#複数データの追加
-# for datas in dataset:
-	# sql = ("INSERT INTO `tweets`(`tweet_id`, `user_id`, `tweet_text`) VALUES (%s,%s,%s)")
-	sql = ("INSERT INTO `rating_final`(`userID`, `placeID`, `rating`, `food_rating`, `service_rating`) VALUES (%s,%s,%s,%s,%s)")
+	sql = ("INSERT INTO `rating`(`userID`, `placeID`, `rating`, `food_rating`, `service_rating`) VALUES (%s,%s,%s,%s,%s)")
 	dbcur.execute(sql, row)
 
 #実際にMySQLに反映させる
