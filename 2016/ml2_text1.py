c4f86087c24d6ea017e530d6f1cbae85b28c59df
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-07-28 21:02:23 +0900

    add codes in class of machine learning 2 in GCI

--- 2016/ml2_text1.py
+++ 2016/ml2_text1.py
@@ -98,14 +98,87 @@ y_hat = clf.predict(test_X)
 print(y_hat)
 print(precision_score(test_y, y_hat))
 
-# column取得
-data2 = pd.read_csv('gci_Feature_user1000.csv')
-columns = data2.columns
-# for col in columns:
-#     col_list = col.split('_')
-#     # if '4month' in col_list or '5month' in col_list or '6month' in col_list:
-#     #     columns = columns.drop(col)
-print 'columns取得'
-print(columns)
-# print 'columns取得'
-# print data2.ix[:3]
+# one-hot-encode
+from sklearn.preprocessing import OneHotEncoder
+# 1~2列目が数値、3~4列目がカテゴリ変数とする
+# 与えらえたデータ中にはないが、3列目のカテゴリ変数は1~4,4列目は1~6とする
+X = np.array([[12,3,4,6],[5,10,1,4],[9,3,2,2],[7,4,3,2]])
+encoder = OneHotEncoder(n_values=[5,7],categorical_features=[2,3], sparse=False)
+
+# これを用いると、カテゴリ変数が左、数値変数が右と順序が変えられてしまう
+X = encoder.fit_transform(X)
+print X
+
+# 正規化
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import numpy as np
+
+X = np.array([[12, 3, 4, 6], [5, 10, 1, 4], [9, 3, 2, 2], [7, 4, 3, 2]], dtype = 'float32')
+print X
+# 平均0, 標準偏差1になるように変換
+scaler = StandardScaler()
+# fit_transformで変換方法を算出し、適用
+# transformでfitした結果を適用
+standard_X = scaler.fit_transform(X)
+print 'standard_X'
+print standard_X
+
+# 最大値1, 最小値0になるように変換
+scaler = MinMaxScaler()
+# fit_transformで変換方法を算出し、適用
+# transformでfitした結果を適用
+MinMax_X = scaler.fit_transform(X)
+print 'MinMax_X'
+print MinMax_X
+
+# ノイズとなる特徴量を除く
+# 一定の値の列を除く
+X = np.array([[1,3,5],[2,3,3],[-1,3,4],[1,3,0]])
+print X
+# np.std():配列の列or行の標準偏差
+# np.delete(array, obj, axis)で削除
+print np.delete(X, np.where(np.std(X, axis=0)==0), 1)
+
+# L1正則化
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X, y = iris.data, iris.target
+print X.shape
+clf = LogisticRegression(C=0.01, penalty="l1")
+clf.fit(X,y)
+
+model = SelectFromModel(clf, prefit=True)
+X_new = model.transform(X)
+print X_new.shape
+
+# 変更重要度による特徴選択
+iris = load_iris()
+X,y = iris.data, iris.target
+print X.shape
+clf = RandomForestClassifier()
+clf.fit(X,y)
+# 変数重要度を表示
+print (clf.feature_importances_)
+
+model = SelectFromModel(clf, prefit=True)
+X_new = model.transform(X)
+print X_new.shape
+
+# パラメータチューニング
+# grid search
+from sklearn.grid_search import ParameterGrid
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# trainデータを分割してvalidationデータをつくる
+train_X, train_y, valid_X, valid_y = train_test_split(X, y, test_size = 0.2, random_state = 1234)
+clf = RandomForestClassifier()
+
+# 探索したいパラメータ
+params = {'n_estimators': range(5,8), 'max_features':[0.8, 0.9, 1.0]}
+param_grid = ParameterGrid(params)
+# 探索したいパラメータのすべての組み合わせが得られている
+print list(param_grid)
