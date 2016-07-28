430218feccafe46e238f183a8c637f81952aba74
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-07-28 20:26:58 +0900

    push ml2-work2.py finished assignment 2 of machine learning 2 in GCI

--- 2016/ml2-work2.py
+++ 2016/ml2-work2.py
@@ -12,16 +12,16 @@ digits = load_digits(n_class=10)
 X, y = digits.data, digits.target
 
 #訓練データとテストデータが8:2になるように分割
-train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=1234)
+train_X, test_X, train_y, test_y = train_test_split(X, Y, test_size=0.2, random_state=1234)
 # 学習器を作る
 clf = LogisticRegression()
 # training
 clf.fit(train_X, train_y)
 # 予測
 pred = clf.predict(test_X)
-print pred
+# print pred
 pred_proba = clf.predict_proba(test_X)
-print pred_proba[:10]
+# print pred_proba[:10]
 # 結果表示
 print 'ロジスティック回帰による予測結果'
 print (precision_score(test_y, pred))
@@ -34,38 +34,33 @@ pred2 = clf2.predict(test_X)
 print 'ランダムフォレストによる予測結果'
 print (precision_score(pred, test_y))
 
-# データをnumpy配列に変換する
-X = np.array(X, dtype="float32")
-y = np.array(y, dtype="float32")
-X_np = X.astype(np.float32)
-y_np = y.astype(np.float32)
 # MinMaxScalerを用いてデータを変形する
 scaler = MinMaxScaler()
-MinMax_X = scaler.fit_transform(X_np)
-MinMax_y = scaler.fit_transform(y_np)
+MinMax_X = scaler.fit_transform(X)
 
 #訓練データとテストデータが8:2になるように分割
-train_X, test_X, train_y, test_y = train_test_split(MinMax_X, MinMax_y, test_size=0.2, random_state=1234)
+train_X2, test_X2, train_y2, test_y2 = train_test_split(MinMax_X, Y, test_size=0.2, random_state=1234)
 # 学習器を作る
-clf = LogisticRegression()
+lr = LogisticRegression()
 # training
-clf.fit(train_X, train_y)
+lr.fit(train_X2, train_y2)
 # 予測
-pred = clf.predict(test_X)
-print pred
-pred_proba = clf.predict_proba(test_X)
-print pred_proba[:10]
+pred = lr.predict(test_X2)
+# print pred
+pred_proba = lr.predict_proba(test_X2)
+# print pred_proba[:10]
+
 # 結果表示
 print 'データ正規化後のロジスティック回帰による予測結果'
-print (precision_score(test_y, pred))
+print (precision_score(test_y2, pred))
 
-clf2 = RandomForestClassifier(random_state = 28)
+rfc = RandomForestClassifier(random_state = 28)
 # training
-clf2.fit(train_X, train_y)
+rfc.fit(train_X2, train_y2)
 # prediction
-pred2 = clf2.predict(test_X)
+pred3 = rfc.predict(test_X2)
 print 'データ正規化後のランダムフォレストによる予測結果'
-print (precision_score(pred, test_y))
+print (precision_score(pred3, test_y2))
 
 
 
