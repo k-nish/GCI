3322af846268907a9f1fb5dc0ce6d004de021808
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-07-29 10:39:08 +0900

    push ml2-work5.py finished assignment 5 of machine learning 2 in GCI

--- 2016/ml2-work5.py
+++ 2016/ml2-work5.py
@@ -1,10 +1,30 @@
+# coding: UTF-8
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score, precision_score
+import numpy as np
 
 iris = load_iris()
 X, y = iris.data, iris.target
 
 skf = StratifiedKFold(y, n_folds=3)
+ans = np.array([])
 for train_index, test_index in skf:
     train_X,test_X = X[train_index], X[test_index]
     train_y,test_y = y[train_index], y[test_index]
+    # 学習器の設定
+    clf = RandomForestClassifier()
+    # 学習
+    clf.fit(train_X,train_y)
+    # 予測
+    pred = clf.predict(test_X)
+    # 予測精度を計算
+    prediction = precision_score(test_y, pred)
+    # 予測をnumpy配列に入れる
+    ans = np.append(ans, prediction)
+
+print 'ans'
+print ans
+print 'ans.mean()'
+print ans.mean()
