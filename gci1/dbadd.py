90a28bf365d5699ae1610a9698c16e4f21a0997f
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-08-10 11:33:04 +0900

    push dbadd.py add code of creating table in mysql

--- gci1/dbadd.py
+++ gci1/dbadd.py
@@ -6,6 +6,14 @@ import csvkit
 dbcon = mysql.connector.connect(database=config.db, user=config.user, password=config.passwd, host=config.host)
 dbcur = dbcon.cursor()
 
+sql1 = "drop table if exists rating;"
+dbcur.execute(sql1);
+print "table削除"
+
+sql2 = "create table rating (userID text, placeID int, rating int, food_rating int, service_rating int);"
+dbcur.execute(sql2);
+print "table作成"
+
 csv_data = csvkit.reader(file('rating_final.csv'))
 for row in csv_data:
 	sql = ("INSERT INTO `rating`(`userID`, `placeID`, `rating`, `food_rating`, `service_rating`) VALUES (%s,%s,%s,%s,%s)")
